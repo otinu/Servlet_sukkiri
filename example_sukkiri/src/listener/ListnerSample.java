351cab63068afd8cba9fa64f93e71d86a5fcd142
otinu <fitshter@gmail.com>
2021-09-22 13:24:56 +0900

    [make]filterSample

--- example_sukkiri/src/listener/ListnerSample.java
+++ example_sukkiri/src/listener/ListnerSample.java
@@ -1,35 +1,18 @@
 package listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-/**
- * Application Lifecycle Listener implementation class ListnerSample
- *
- */
+
 @WebListener
 public class ListnerSample implements ServletContextListener {
-
-    /**
-     * Default constructor. 
-     */
-    public ListnerSample() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-
-	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0)  { 
-         // TODO Auto-generated method stub
-    }
-	
+    	public void contextInitialized(ServletContextEvent sce) {
+    		ServletContext context = sce.getServletContext();
+    		Integer count = 0;
+    		context.setAttribute("count", count);
+    	}
+	    public void contextDestroyed(ServletContextEvent arg0)  {
+	    }
 }
